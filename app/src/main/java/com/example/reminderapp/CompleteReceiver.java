2757eb3389c92d788de1541b736f12e60ad311c0
rajviparmar0302 <rajviparmar0302@gmail.com>
2025-09-10 19:59:45 +0530

    Auto hide notification on Complete

--- app/src/main/java/com/example/reminderapp/CompleteReceiver.java
+++ app/src/main/java/com/example/reminderapp/CompleteReceiver.java
@@ -1,5 +1,6 @@
 package com.example.reminderapp;
 
+import android.app.AlarmManager;
 import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -15,6 +16,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import android.app.PendingIntent;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,18 +24,32 @@ import java.util.Map;
 public class CompleteReceiver extends BroadcastReceiver {
 
     private static final int NOTIFICATION_ID = 100;
+    private static final int REMINDER_REQUEST_CODE = 100;
 
     @Override
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context, "Reminder Completed!", Toast.LENGTH_SHORT).show();
 
-        // Cancel notification
+        // Cancel the alarm
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        if (alarmManager != null) {
+            Intent alarmIntent = new Intent(context, ReminderReceiver.class);
+            PendingIntent alarmPendingIntent = PendingIntent.getBroadcast(
+                    context,
+                    REMINDER_REQUEST_CODE,  // same request code
+                    alarmIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
+            );
+            alarmManager.cancel(alarmPendingIntent);
+        }
+
+        // Cancel the notification
         NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         if (manager != null) {
             manager.cancel(NOTIFICATION_ID);
         }
 
-        // Save event in Firestore
+
         saveReminderEvent("Reminder Completed");
     }
 
